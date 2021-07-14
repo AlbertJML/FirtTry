f3497ba8b361b99a8ab35a931239ad2b9152c345
AlbertJML <43562315+AlbertJML@users.noreply.github.com>
2021-07-14 16:28:35 -0500

    finish workshop

--- src/explore_bike_data.R
+++ src/explore_bike_data.R
@@ -78,3 +78,139 @@ bike_dates %>%
                 date_num = as.Date(date_num, origin ="1970-01-01"),
                 date_char = as.Date(date_char)
 )
+
+
+## Additional filtering and Selecting
+bike %>% select(dteday, cnt)
+bike %>% select(dteday, cnt, temp) %>% select(-temp)
+keep_vars <- c("dteday", "cnt","temp")
+keep_vars <- paste0("temp", 1:12)
+bike %>% select(all_of(keep_vars))
+
+### Filtering
+bike %>% filter(season =="spring")
+#'! ='
+bike %>% filter(season != "spring")%>% select(season)
+
+bike %>% 
+        filter(season != "spring")%>%
+        select(season) %>%
+        distinct()
+
+bike %>% 
+        filter(season =="summer"| season=="winter")
+
+seasons <- c("summer","winter")
+
+bike %>%
+        filter(season %in% seasons)
+
+
+## More dplyr verbs
+# summarize: summary of multiple rows for a col/variable
+# group_by: perform an operation separately for each group
+bike2 %>% 
+        filter(season=="summer")%>% 
+        summarize(
+                temp_mean = mean(temp),
+                cnt_mean = mean(cnt),
+                cnt_sum = sum(cnt)
+        )
+
+bike2 %>% 
+        group_by(season) %>%
+        summarize(
+                temp_mean = mean(temp),
+                ride_sum = sum(cnt)
+        )
+
+# What are the season definitions?
+# Create a new season with meteorological definitions
+sort(names(bike))
+bike %>% select(season, mnth) %>%distinct()
+
+bike3 <- bike2 %>% 
+        mutate(
+                season2 = 1*(mnth %in% c("December", "January","February"))+
+                        2*(mnth %in% c("March", "April","May"))+
+                        3*(mnth %in% c("June", "July","August"))+
+                        4*(mnth %in% c("September", "October","November"))
+                )%>% 
+        mutate(
+                season2 = factor(season2, levels = 0:4,
+                                 labels = c("Unknown","Winter","Spring","Summer","Fall"))
+        )
+        
+bike3$season2
+
+bike3 %>% 
+        group_by(season2) %>%
+        summarize(
+                temp_mean = mean(temp),
+                ride_sum = sum(cnt)
+        )
+
+## Facetting in ggplot
+
+ggplot(data=bike3)+
+        geom_point(aes(x=temp, y=cnt))+
+        geom_smooth(aes(x=temp, y=cnt),
+                    method = "lm",
+                    formula = y ~ poly(x,2))+
+        facet_wrap(~season2)
+
+## Pivoting wider to long and longer to wide
+# Long to wide: Data in multiple columns e.g. temp1, temp2,...temp12
+# Wide to long: data in one column, clasifier in other columns 
+# tidyr is the package that allow transformations
+months <- c("January","February","March", "April",
+            "May","June", "July","August",
+            "September","October","November","December")
+
+tidybike <- bike3 %>% 
+        select(yr, mnth, temp, cnt) %>% 
+        mutate(month=factor(mnth,
+                            levels = months,
+                            labels = months))%>% 
+        group_by(yr, month) %>% 
+        summarize(temp_mean=mean(temp),
+                  rides = sum(cnt))
+
+#Tidyr functions for long to wide
+# spread
+# pivot_wider
+tidybike %>% 
+        select(-rides) %>%
+        pivot_wider(values_from = temp_mean,
+                         names_from = month,
+                         names_prefix = "temp_")
+
+tidybike %>% 
+        select(-rides) %>%
+        tidyr::spread(key = month, value = temp_mean)
+        
+
+rides <- tidybike %>%
+                select(-temp_mean) %>%
+                pivot_wider(values_from = rides,
+                            names_from = month,
+                            names_prefix = "rides_")%>%
+        rename_with(tolower) %>%
+        rename(year=yr)
+
+     
+## Going from wide to long
+# pivot_longer
+# gather
+rides %>% gather(key = "month", value = "rides",-year)
+
+rides %>% 
+        select(year, rides_january, rides_february) %>%
+        pivot_longer(names_to = "month", 
+                     values_to = "rides",
+                     cols = c("rides_january","rides_february")) %>% 
+        mutate(month = substr(month, 7, nchar(month)))%>% 
+        mutate(month = toupper(month))
+
+
+        
